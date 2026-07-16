63fa4fcbfa05ac4ff73c3f7a3f97567d0319e2f2
Jul1axx <jerwinedeso21@gmail.com>
2026-07-16 13:12:13 +0800

    java branches first commit

---
+++ week1/Quizes.java
@@ -0,0 +1,13 @@
+public class Quizes {
+
+public static void main(String[] args) {
+
+    int a = 80;
+    int b = 21;    
+    int total = a + b;
+
+    System.out.println("Total: " + total);
+
+
+}
+}
